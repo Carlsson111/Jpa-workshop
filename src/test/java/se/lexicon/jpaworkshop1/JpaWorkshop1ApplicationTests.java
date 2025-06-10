68b70707d61f037a1774e1465d279da069e33636
Carlsson111 <Linusjaa1@gmail.com>
2025-06-10 09:44:36 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/jpaworkshop1/JpaWorkshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshop1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
